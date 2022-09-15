82a6d0e182e265bf53ce8e0425ed1baeb0eda7c3
Jerilyn <jerilyn.wise@paddywax.com>
2022-09-15 17:32:25 -0500

    client side framework completed/ styling used from Mern project but will be replace with another CSS library once it is active and I am able to see changes made

---
+++ client/src/components/WorkoutList/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const WorkoutList = ({ workouts, title }) => {
+  if (!workouts.length) {
+    return <h3>No Workouts Yet</h3>;
+  }
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      {workouts &&
+       workouts.map(workout => (
+          <div key={workout._id} className="card mb-3">
+            <p className="card-header">
+              <Link
+                to={`/profile/${workout.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {workout.username}
+              </Link>{' '}
+              Workout on {workout.createdAt}
+            </p>
+            <div className="card-body">
+              <Link to={`/workout/${workout._id}`}>
+                <p>{workout.workoutText}</p>
+                <p className="mb-0">
+                  Reactions: {workout.reactionCount} || Click to{' '}
+                  {workout.reactionCount ? 'see' : 'start'} the discussion!
+                </p>
+              </Link>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default WorkoutList;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import WorkoutList from '../components/WorkoutList';
+import WorkoutForm from '../components/WorkoutForm';
+import FollowerList from '../components/FollowerList';
+
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_WORKOUTS, QUERY_ME_BASIC } from '../utils/queries';
+import FollowerList from '../components/FollowerList';
+
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_WORKOUTS);
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+  const workouts = data?.workouts || [];
+
+  const loggedIn = Auth.loggedIn();
+
+  return (
+    <main>
+      <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <WorkoutForm />
+          </div>
+        )}
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <WorkoutList
+              workouts={workouts}
+              title="Some Feed for Workout(s)..."
+            />
+          )}
+        </div>
+        {loggedIn && userData ? (
+          <div className="col-12 col-lg-3 mb-3">
+            <FollowerList
+              username={userData.me.username}
+              followerCount={userData.me.followerCount}
+              followers={userData.me.followers}
+            />
+          </div>
+        ) : null}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
